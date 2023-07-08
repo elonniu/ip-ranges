4b3df556d4c2701a0778a0fcc5b7d68a12328827
elonniu <elonniu@amazon.com>
2023-07-08 09:47:55 +0800

    update sst

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -1,17 +1,38 @@
 import axios from "axios";
-import {feishu} from "sst-helper";
+import {feishu, jsonResponse} from "sst-helper";
+import {DynamoDB} from "aws-sdk";
+import {Table} from "sst/node/table";
+
+const dynamoDb = new DynamoDB.DocumentClient();
+const TableName = Table.IpRanges.tableName;
 
 export async function handle() {
 
     const ranges_url = 'https://ip-ranges.amazonaws.com/ip-ranges.json';
     const {data} = await axios.get(ranges_url);
 
+    // get item from dynamodb
+    const history = {
+        TableName,
+        Key: {
+            id: 'ip_ranges'
+        }
+    }
+    const {Item} = await dynamoDb.get(history).promise();
+
     const prefixes = data.prefixes;
 
     let ip_service: Record<string, number> = {};
+    let ip_service_diff: Record<string, number> = {};
+
     let ip_region: Record<string, number> = {};
+    let ip_region_diff: Record<string, number> = {};
+
     let prefix_num: Record<string, number> = {};
+    let prefix_num_diff: Record<string, number> = {};
+
     let ip_sum = 0;
+    let ip_sum_diff = 0;
 
     for (const prefix of prefixes) {
         prefix.service = prefix.service.toLowerCase();
@@ -34,12 +55,45 @@ export async function handle() {
         ip_region[prefix.region] += ipRange;
         ip_service[prefix.service] += ipRange;
         ip_sum += ipRange;
+
+        if (Item) {
+            if (Item.ip_region[prefix.region]) {
+                ip_region_diff[prefix.region] = Item.ip_region[prefix.region] - ip_region[prefix.region];
+                ip_region_diff[prefix.region] === 0 && delete ip_region_diff[prefix.region];
+            }
+            if (Item.ip_service[prefix.service]) {
+                ip_service_diff[prefix.service] = Item.ip_service[prefix.service] - ip_service[prefix.service];
+                ip_service_diff[prefix.service] === 0 && delete ip_service_diff[prefix.service];
+            }
+            if (Item.prefix_num[prefix.region]) {
+                prefix_num_diff[prefix.region] = Item.prefix_num[prefix.region] - prefix_num[prefix.region];
+                prefix_num_diff[prefix.region] === 0 && delete prefix_num_diff[prefix.region];
+            }
+            ip_sum_diff = Item.ip_sum - ip_sum;
+        }
     }
+
+    // write to dynamodb
+    const params = {
+        TableName,
+        Item: {
+            id: 'ip_ranges',
+            ip_service,
+            ip_region,
+            prefix_num,
+            ip_sum
+        }
+    };
+    await dynamoDb.put(params).promise();
+
 
     // order ip_num by value desc
     prefix_num = Object.fromEntries(Object.entries(prefix_num).sort(([, a], [, b]) => b - a));
     ip_service = Object.fromEntries(Object.entries(ip_service).sort(([, a], [, b]) => b - a));
     ip_region = Object.fromEntries(Object.entries(ip_region).sort(([, a], [, b]) => b - a));
+    prefix_num_diff = Object.fromEntries(Object.entries(prefix_num).sort(([, a], [, b]) => b - a));
+    ip_service_diff = Object.fromEntries(Object.entries(ip_service).sort(([, a], [, b]) => b - a));
+    ip_region_diff = Object.fromEntries(Object.entries(ip_region).sort(([, a], [, b]) => b - a));
 
     //  Format the ip_service value number to thousands separator.
     for (const [key, value] of Object.entries(prefix_num)) {
@@ -54,12 +108,22 @@ export async function handle() {
         ip_region[key] = value.toLocaleString('en-US');
     }
 
-    await feishu({
+    const json = {
         ranges_url,
+        diff: {
+            ip_sum_diff: ip_sum_diff.toLocaleString('en-US'),
+            ip_region_diff: Object.keys(ip_region_diff) ? undefined : ip_region_diff,
+            ip_service_diff: Object.keys(ip_service_diff) ? undefined : ip_service_diff,
+            prefix_num_diff: Object.keys(prefix_num_diff) ? undefined : prefix_num_diff,
+        },
         ip_sum: ip_sum.toLocaleString('en-US'),
         prefix_num,
         ip_service,
-        ip_region
-    });
+        ip_region,
+    };
+
+    await feishu(json);
+
+    return jsonResponse(json);
 
 }
--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,25 +1,36 @@
-import {Function, StackContext, Topic} from "sst/constructs";
-import {lambdaUrl} from "sst-helper";
+import {Function, StackContext, Table, Topic} from "sst/constructs";
+import {ddbUrl, lambdaUrl} from "sst-helper";
 import * as sns from "aws-cdk-lib/aws-sns";
 
 export function API({stack, app}: StackContext) {
 
     // create sns topic by arn
-    const topic = sns.Topic.fromTopicArn(stack, "AmazonIpSpaceChanged", "arn:aws:sns:us-east-1:806199016981:AmazonIpSpaceChanged");
+    const topic = sns.Topic.fromTopicArn(
+        stack,
+        "AmazonIpSpaceChanged",
+        "arn:aws:sns:us-east-1:806199016981:AmazonIpSpaceChanged"
+    );
 
-    const fun = new Function(
-        stack, 'Function',
+    const table = new Table(stack, "IpRanges", {
+        fields: {
+            id: "string",
+        },
+        primaryIndex: {partitionKey: "id"},
+    });
+
+    const fun = new Function(stack, 'Function',
         {
             runtime: 'nodejs18.x',
             handler: "packages/functions/src/lambda.handle",
-            memorySize: 4028,
+            memorySize: 2048,
             timeout: 30,
+            bind: [table],
             environment: {
                 FEISHU_ID: process.env.FEISHU_ID || '',
             }
         });
 
-    new Topic(stack, "Topic", {
+    !app.local && new Topic(stack, "Topic", {
         subscribers: {
             subscriber1: fun,
         },
@@ -29,6 +40,7 @@ export function API({stack, app}: StackContext) {
     });
 
     stack.addOutputs({
-        lambdaUrl: lambdaUrl(fun, app)
+        lambdaUrl: lambdaUrl(fun.functionName, stack.region),
+        table: ddbUrl(table.tableName, stack.region)
     });
 }
